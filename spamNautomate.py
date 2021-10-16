dd7d27f94d3af167a75c8b7a0a222f55c0e9af1a
Romeo <Romeobusiness310@gmail.com>
2021-10-16 13:55:14 +0400

    added spamTexts function

--- spamNautomate.py
+++ spamNautomate.py
@@ -1,5 +1,17 @@
 import pyautogui
+from time import sleep
 
-for i in range(100):
-    pyautogui.typewrite('Follow EmiratiCoder!!')
-    pyautogui.press('enter')
+def spamTexts():
+    numberOfMessages = int(input('How many messages do you want to send?'))
+
+    if numberOfMessages > 100:
+        print('You can not send more than 100 messages')
+    else:
+        for i in range(3, 0, -1):
+            print(i)
+            sleep(1)
+        for i in range(numberOfMessages):
+            pyautogui.typewrite('Follow EmiratiCoder!!')
+            pyautogui.press('enter')
+
+spamTexts()
